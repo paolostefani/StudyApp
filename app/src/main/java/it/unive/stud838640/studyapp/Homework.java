3dc31ac485cc0f3914c5c3ceda56f523d3da6eba
paolostefani <aliveps@gmail.com>
2015-02-15 19:24:12 +0100

    Initial commit

---
+++ app/src/main/java/it/unive/stud838640/studyapp/Homework.java
@@ -0,0 +1,69 @@
+package it.unive.stud838640.studyapp;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by paolo on 13/02/15.
+ */
+public class Homework {
+    private String name, description, subject, timeLeft;
+    private Date expiry;
+    private int percentage;
+    private String leader; // TODO change String type to Team type
+    private ArrayList<Task> tasks;
+
+    public Homework(String name) {
+        this.name = name;
+        leader = "You";
+        tasks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Date getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(Date expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getTimeLeft() {
+        return timeLeft;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
---
+++ app/src/main/java/it/unive/stud838640/studyapp/Task.java
@@ -0,0 +1,7 @@
+package it.unive.stud838640.studyapp;
+
+/**
+ * Created by paolo on 15/02/15.
+ */
+public class Task {
+}
