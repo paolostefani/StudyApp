3f260687dfb1c66668a4d2fc4ed773ca78702366
paolostefani <aliveps@gmail.com>
2015-02-21 15:47:59 +0100

    Working on HomeworkListFragment: manage GridView

--- app/src/main/java/it/unive/stud838640/studyapp/HomeworkListFragment.java
+++ app/src/main/java/it/unive/stud838640/studyapp/HomeworkListFragment.java
@@ -1,15 +1,17 @@
 package it.unive.stud838640.studyapp;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,23 +19,26 @@ import java.util.ArrayList;
 /**
  * Created by paolo on 18/02/15.
  */
-public class HomeworkListFragment extends ListFragment {
+public class HomeworkListFragment extends Fragment {
     private ArrayList<Homework> homeworks;
+    private GridView hwGridView;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().setTitle(R.string.homeworks_title);
-        homeworks = HomeworkManager.get(getActivity()).getHomeworks();
-        HomeworkAdapter hwAdapter = new HomeworkAdapter(homeworks);
-        setListAdapter(hwAdapter);
+
     }
 
+    @Nullable
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        Homework hw = ((HomeworkAdapter) getListAdapter()).getItem(position);
-        Log.d("HomeworkListFragment", hw.getName());
-        Toast.makeText(getActivity(), hw.getName(), Toast.LENGTH_SHORT).show();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_homework_grid, container, false);
+        hwGridView = (GridView) v.findViewById(R.id.hwork_gridview);
+        homeworks = HomeworkManager.get(getActivity()).getHomeworks();
+        hwGridView.setAdapter(new HomeworkAdapter(homeworks));
+        
+        return v;
     }
 
     private class HomeworkAdapter extends ArrayAdapter<Homework> {
@@ -49,7 +54,7 @@ public class HomeworkListFragment extends ListFragment {
                         .inflate(R.layout.list_item_homework, null);
             }
 
-            Homework hw = getItem(position);
+            final Homework hw = getItem(position);
             Button hworkButton = (Button) convertView.findViewById(R.id.hwork_button);
             hworkButton.setText(hw.getId() + "");
             GradientDrawable bgShape = (GradientDrawable) hworkButton.getBackground();
@@ -60,6 +65,13 @@ public class HomeworkListFragment extends ListFragment {
                 color = "#0000ff";
             bgShape.setColor(Color.parseColor(color));
 
+            hworkButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("HomeworkListFragment", hw.getName());
+                    Toast.makeText(getActivity(), hw.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
 
             return convertView;
         }
